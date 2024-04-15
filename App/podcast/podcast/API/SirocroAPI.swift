532742bebc34ec5c2a0c94ffa6abdd504cc0a2a5
HIEUNT <nth.nguyenhieu130220@gmail.com>
2024-04-15 16:56:25 +0700

    Update SirocroAPI.swift

--- App/podcast/podcast/API/SirocroAPI.swift
+++ App/podcast/podcast/API/SirocroAPI.swift
@@ -68,59 +68,59 @@ struct SirocroNetwork {
         plugins: plugins
     )
     
-    static func request(
-        target: SirocroAPI,
-        success successCallback: @escaping (_ json: JSON?, _ allHeaderFields: [AnyHashable : Any]?) -> Void,
-        progress progressCallback: ((_ value: Double) -> Void)? = nil,
-        error errorCallback: @escaping (_ statusCode: Int) -> Void,
-        failure failureCallback: @escaping (Moya.MoyaError) -> Void) -> Cancellable {
-            return provider.request(target, callbackQueue: self.queue, progress: { response in
-                progressCallback?(response.progress)
-            }) { result in
-                switch result {
-                case let .success(response):
-                    let headerFields = response.response?.allHeaderFields
-                    do {
-                        let res = try response.filterSuccessfulStatusAndRedirectCodes()
-                        if (res.statusCode >= 300) {
-                            successCallback(nil, headerFields)
-                            return
-                        }
-                        let json = try JSON(response.mapJSON())
-                        successCallback(json, headerFields)
-                    } catch let error {
-                        if response.statusCode == 200 {
-                            successCallback(nil, headerFields)
-                        }
-                        switch error as! Moya.MoyaError {
-                        case .statusCode(let response):
-                            if let statusCode = StatusCode(rawValue: response.statusCode) {
-                                switch statusCode {
-                                case .unauthorized, .forbidden:
-                                    break
-                                case .serviceUnavailable:
-                                    break
-                                default:
-                                    DispatchQueue.main.async {
-                                        errorCallback(statusCode.rawValue)
-                                    }
-                                }
-                            } else {
+    static func request(target: SirocroAPI,
+                        success successCallback: @escaping (_ json: JSON?, _ allHeaderFields: [AnyHashable : Any]?) -> Void,
+                        progress progressCallback: ((_ value: Double) -> Void)? = nil,
+                        error errorCallback: @escaping (_ statusCode: Int) -> Void,
+                        failure failureCallback: @escaping (Moya.MoyaError) -> Void) -> Cancellable {
+        return provider.request(target, callbackQueue: self.queue,
+                                progress: { response in
+            progressCallback?(response.progress)
+        }) { result in
+            switch result {
+            case let .success(response):
+                let headerFields = response.response?.allHeaderFields
+                do {
+                    let res = try response.filterSuccessfulStatusAndRedirectCodes()
+                    if (res.statusCode >= 300) {
+                        successCallback(nil, headerFields)
+                        return
+                    }
+                    let json = try JSON(response.mapJSON())
+                    successCallback(json, headerFields)
+                } catch let error {
+                    if response.statusCode == 200 {
+                        successCallback(nil, headerFields)
+                    }
+                    switch error as! Moya.MoyaError {
+                    case .statusCode(let response):
+                        if let statusCode = StatusCode(rawValue: response.statusCode) {
+                            switch statusCode {
+                            case .unauthorized, .forbidden:
+                                break
+                            case .serviceUnavailable:
+                                break
+                            default:
                                 DispatchQueue.main.async {
-                                    failureCallback(error as! MoyaError)
+                                    errorCallback(statusCode.rawValue)
                                 }
                             }
-                        default:
+                        } else {
                             DispatchQueue.main.async {
-                                failureCallback(error as! Moya.MoyaError)
+                                failureCallback(error as! MoyaError)
                             }
                         }
+                    default:
+                        DispatchQueue.main.async {
+                            failureCallback(error as! Moya.MoyaError)
+                        }
                     }
-                case let .failure(error):
-                    DispatchQueue.main.async {
-                        failureCallback(error)
-                    }
+                }
+            case let .failure(error):
+                DispatchQueue.main.async {
+                    failureCallback(error)
                 }
             }
         }
+    }
 }
